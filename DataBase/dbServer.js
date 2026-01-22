cd627296c9db741a8258495fddae199f8e13104c
itsmohdzaheer <zaheershaik0323@gmail.com>
2026-01-22 10:36:40 +0530

    CRUD WITH MONGO

--- DataBase/dbServer.js
+++ DataBase/dbServer.js
@@ -8,13 +8,54 @@ dbConnect();
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get("/", async (req, res) => {
+  let search = req.query.search ? req.query.search : "";
+  let students = await Student.find({
+    $or:[
+      {firstName :{$regex: search, $options: "i" }}, // i is the Mongoose /i check the docmentation
+      {lastName :{$regex: search, $options: "i" }},
+      {course :{$regex: search, $options: "i" }},
+      {email :{$regex: search, $options: "i" }}
+    ]
+  });
+  res.render("index", { students })
+})
 app.post("/add-student", async (req, res) => {
   let student = await Student.create(req.body);
   console.log(student);
   return res.redirect("/");
+});
+
+app.get("/edit-student/:id", async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    if (!student) return res.redirect("/");
+    res.render("edit", { student });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
+
+app.get("/delete-student/:id", async (req, res) => {
+  let id = req.params.id;
+  let student = await Student.findById(id);
+
+  if(!student){
+    console.log('student not found');
+    return res.redirect('/')
+  }
+  await Student.findByIdAndDelete(id)
+  return res.redirect("/")
+})
+app.post("/update-student/:id", async (req, res) => {
+  try {
+    await Student.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
 });
 app.listen(PORT, () => {
   console.log(`Server start at http://localhost:${PORT}`);
